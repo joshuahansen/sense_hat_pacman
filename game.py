b7c47c19243e6d162f26874dffbaa7e41be68828
Josh <joshua.hansen188@gmail.com>
2018-07-26 06:29:03 +0000

    game working with food

--- game.py
+++ game.py
@@ -1,21 +1,25 @@
 from sense_hat import SenseHat
 import time
 import random
-import Board
-import Player
+from board import Board
+from player import Player
 
 class Game():
     def __init__(self):
         self.board = Board()
-        self.player = Player((0, 255, 0), (1, 1))
+        self.player = Player(self.board, (0, 255, 0), (1, 1))
 
-    def play():
+    def play(self):
         self.board.display_board()
         self.player.display_player()
 
         while True:
-            self.player.get_move
+            self.player.get_move()
+
+    def reset(self):
+        self.board.clear_display()
 
 game = Game()
 
+game.reset()
 game.play()
